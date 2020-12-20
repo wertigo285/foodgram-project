261c7c6b264dd91bb32fc547627e97bcc36252c7
wertigo285 <wertuoz574@gmail.com>
2020-12-20 07:57:55 +0300

    Закончил с формой рецепта, подписками, избранным

--- recipes/models.py
+++ recipes/models.py
@@ -1,24 +1,34 @@
-from django.db import models
+from datetime import datetime
+
+
 from django.contrib.auth import get_user_model
+from django.db import models
 from django.utils.text import slugify
+
+
 from unidecode import unidecode
 
+
 User = get_user_model()
+
 
 class Recipe(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='recipes')
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='recieps/', blank=True, null=True)
+    image = models.ImageField(upload_to='recieps/')
     description = models.TextField()
     ingredients = models.ManyToManyField(
         'Ingredient', through='IngredientQuantity')
-    tags = models.ManyToManyField('Tag', related_name='recipes')
+    tags = models.ManyToManyField('Tag', related_name='recipes', blank=True)
     duration = models.PositiveSmallIntegerField()
 
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.title
 
     def get_duration(self):
         result = ''
@@ -35,15 +45,26 @@ class Ingredient(models.Model):
     title = models.CharField(max_length=100)
     dimension = models.CharField(max_length=20)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'dimension'], name='unique_ingredient')
+        ]
+
+    def __str__(self):
+        return self.title
+
 
 class IngredientQuantity(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
+    def __str__(self):
+        return f'{self.recipe.title} ({self.ingredient.title} - {self.quantity} {self.ingredient.dimension})'
+
 
 class Tag(models.Model):
-
     title = models.CharField(max_length=20)
     color = models.CharField(max_length=20, null=False, default='', blank=True)
     slug = models.SlugField(max_length=20)
@@ -52,12 +73,24 @@ class Tag(models.Model):
         self.slug = slugify(unidecode(self.title))
         super(Tag, self).save(*args, **kwargs)
 
+    def __str__(self):
+        return self.title
+
 
 class Subscription(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='followers')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='followings')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'], name='unique_subscription')
+        ]
+
+    def __str__(self):
+        return f'Подписка {self.user.username} на {self.author.username}'
 
 
 class Favorite(models.Model):
@@ -66,10 +99,27 @@ class Favorite(models.Model):
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name='users_favorite')
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'], name='unique_favorite')
+        ]
+
+    def __str__(self):
+        return f'Рецепт {self.recipe} в избранном {self.user.username}'
+
 
 class ShoppingList(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='shopping_lists')
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name='shopping_lists')
-    
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'], name='unique_position')
+        ]
+
+    def __str__(self):
+        return f'Рецепт {self.recipe} в избранном {self.user.username}'
--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,28 @@
+import json
+
+
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.transaction import atomic
+from django.db.models import Count, OuterRef, Prefetch, Subquery
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.http import require_http_methods
 
 
-from .models import (Recipe, Tag, User, Subscription)
+from .forms import RecipeForm
+from .models import (Favorite, Ingredient, IngredientQuantity,
+                     Recipe, Subscription, ShoppingList, Tag, User)
+from .utilities import set_ingredients
 
 
 def index(request):
-
     tags = Tag.objects.all()
 
     recipes = Recipe.objects.select_related(
-        'author').prefetch_related('tags').all()
-    paginator = Paginator(recipes, 6)
+        'author').prefetch_related('tags')
 
+    paginator = Paginator(recipes, 6)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
@@ -21,16 +30,16 @@ def index(request):
                'tags': tags, 'title': 'Рецепты'}
     return render(request, 'index.html', context=context)
 
-def author_view(request,author_id):
 
-    author = get_object_or_404(User,pk=author_id)
-
+def author_view(request, author_id):
     tags = Tag.objects.all()
 
-    recipes = author.recipes.select_related(
-        'author').prefetch_related('tags').all()
-    paginator = Paginator(recipes, 6)
+    author = get_object_or_404(User, pk=author_id)
 
+    recipes = author.recipes.select_related(
+        'author').prefetch_related('tags')
+
+    paginator = Paginator(recipes, 6)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
@@ -38,17 +47,17 @@ def author_view(request,author_id):
                'tags': tags, 'title': author.first_name}
     return render(request, 'index.html', context=context)
 
+
 @login_required
 def favorites(request):
+    tags = Tag.objects.all()
 
     user = request.user
 
-    tags = Tag.objects.all()
-
     recipes = Recipe.objects.select_related('author').prefetch_related(
         'tags').filter(users_favorite__user=user)
-    paginator = Paginator(recipes, 6)
 
+    paginator = Paginator(recipes, 6)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
@@ -56,27 +65,126 @@ def favorites(request):
                'tags': tags, 'title': 'Избранное'}
     return render(request, 'index.html', context=context)
 
+
 @login_required
 def subscriptions(request):
-
     user = request.user
-    authors = User.objects.prefetch_related('recipes').filter(followings__user=user)
 
-    
+    # Лимитирование запроса генерируемого prefetch_related
+    # https://code.djangoproject.com/ticket/26780#comment:6
+    feed_length = 3
+    authors = User.objects.filter(
+        followings__user=user).annotate(recipe_count=Count('recipes')).prefetch_related(
+        Prefetch('recipes', queryset=Recipe.objects.filter(id__in=Subquery(Recipe.objects.filter(
+            author_id=OuterRef('author_id')).values_list('id', flat=True)[:feed_length]))))
+
     paginator = Paginator(authors, 6)
-
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
     context = {'page': page, 'paginator': paginator,
-                'title': 'Мои подписки'}
+               'feed_length': feed_length, 'title': 'Мои подписки'}
     return render(request, 'follow.html', context=context)
 
-def recipe_view(request, recipe_id):
 
+def recipe_view(request, recipe_id):
     recipe = get_object_or_404(Recipe.objects.select_related('author').prefetch_related(
         'tags', 'ingredients', 'ingredientquantity_set'), pk=recipe_id)
+
     ingredients = zip(recipe.ingredients.all(),
                       recipe.ingredientquantity_set.all())
 
     return render(request, 'single_page.html', context={'recipe': recipe, 'ingredients': ingredients})
+
+
+@login_required
+def recipe_edit(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+
+    if request.user != recipe.author:
+        return redirect(to='recipe', recipe_view=recipe_id)
+
+    form = RecipeForm(request.POST or None,
+                      files=request.FILES or None, instance=recipe)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            with atomic():
+                recipe.save()
+                set_ingredients(request.POST, recipe)
+                form.save_m2m()
+            return redirect(to='recipe', recipe_id=recipe.pk)
+
+    ingredients = list(IngredientQuantity.objects.filter(recipe=recipe).select_related(
+        'ingredient').values('pk', 'quantity', 'ingredient__title', 'ingredient__dimension').order_by('pk'))
+
+    return render(request, 'recipe_form.html', context={'form': form, 'ingredients': ingredients})
+
+@login_required
+def recipe_delete(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+
+    if request.user != recipe.author:
+        return redirect(to='recipe', recipe_view=recipe_id)
+
+    recipe.delete()
+
+    return redirect(to='index')
+
+
+@login_required
+def recipe_new(request):
+    form = RecipeForm(request.POST or None, files=request.FILES or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            with atomic():
+                recipe.save()
+                set_ingredients(request.POST, recipe)
+                form.save_m2m()
+            return redirect(to='recipe', recipe_id=recipe.pk)
+
+    return render(request, 'recipe_form.html', context={'form': form})
+
+
+@login_required
+@require_http_methods(['GET'])
+def ingredients_js(request):
+    query = request.GET['query']
+    data = Ingredient.objects.filter(
+        title__icontains=query).values('title', 'dimension')
+
+    return JsonResponse(list(data), safe=False)
+
+
+@login_required
+@require_http_methods(['POST', 'DELETE'])
+def favorites_js(request, recipe_id=None):
+    user = request.user
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        recipe = get_object_or_404(Recipe, pk=data['id'])
+        Favorite.objects.create(user=user, recipe=recipe)
+    else:
+        fav = get_object_or_404(Favorite, user=user, recipe__id=recipe_id)
+        fav.delete()
+
+    return JsonResponse(data={})
+
+
+@login_required
+@require_http_methods(['POST', 'DELETE'])
+def subscriptions_js(request, author_id=None):
+    user = request.user
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        author = get_object_or_404(User, pk=data['id'])
+        Subscription.objects.create(user=user, author=author)
+    else:
+        sub = get_object_or_404(Subscription, user=user, author__id=author_id)
+        sub.delete()
+
+    return JsonResponse(data={})
